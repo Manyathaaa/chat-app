1ffc2c3c6f995f8874eeb5ad26143cf467229667
manyathaaa <mailtomanyatha@gmail.com>
2025-12-06 19:13:04 +0530

    Add initial server setup, chat data, and configuration files

---
+++ backend/data/data.js
@@ -0,0 +1,80 @@
+const chats =[
+    {
+        "isGroupChat" : false,
+        "users":  [
+            {
+                "name": "Alice",
+                "email": "alice@example.com"
+            },
+            {
+                "name": "Bob",
+                "email": "bob@example.com"
+            }
+        ],
+        "_id": "61f7c0e8b4d1c8b4f0a1e9c1",
+        "chatName": "Alice"
+    },
+
+    {
+        "isGroupChat" : false,
+        "users":  [
+            {
+                "name": "shree",
+                "email": "shree@example.com"
+            },
+            {
+                "name": "tina",
+                "email": "tina@example.com"
+            }
+        ],
+        "_id": "61f7c0e8b4d1c8b4f0a1e8c4",
+        "chatName": "shree"
+    },
+    {
+        "isGroupChat" : false,
+        "users":  [
+            {
+                "name": "sinu",
+                "email": "sinu@example.com"
+            },
+            {
+                "name": "krish",
+                "email": "krish@example.com"
+            }
+        ],
+        "_id": "61f7c0e8b4d1c8b4f0a1f6gh",
+        "chatName": "sinu"
+    },
+    {
+        "isGroupChat" : false,
+        "users":  [
+            {
+                "name": "tom",
+                "email": "tom@example.com"
+            },
+            {
+                "name": "jerry",
+                "email": "jerry@example.com"
+            }
+        ],
+        "_id": "61f7c0e8b4d1c8b4f0a1e9z3",
+        "chatName": "tom"
+    },
+    {
+        "isGroupChat" : false,
+        "users":  [
+            {
+                "name": "nobi",
+                "email": "nobi@example.com"
+            },
+            {
+                "name": "sizi",
+                "email": "sizi@example.com"
+            }
+        ],
+        "_id": "61f7c0e8b4d1c8b4f0a1e8vf",
+        "chatName": "nobi"
+    },
+]
+
+module.exports = {chats};
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {chats} from './data/data.js'; 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/api/v1/chats', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/v1/chats/:id', (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+}); 
