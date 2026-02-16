c5ae713678e7064b9687e31e9651da9d60794452
manyathaaa <mailtomanyatha@gmail.com>
2026-02-16 21:18:20 +0530

    style: update App.css for improved styling and structure; add HomePage route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/chats" component={ChatPage} />
+        <Route path="/" component={HomePage} exact />
       </Switch>
     </BrowserRouter>
   );
