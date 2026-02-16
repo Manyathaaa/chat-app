7b3fddd078e9e0a155d3460a938a4a356d1e86e2
manyathaaa <mailtomanyatha@gmail.com>
2026-02-16 22:30:51 +0530

    feat: enhance HomePage layout with Chakra UI components and update App structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
+  <div className="App">
     <BrowserRouter>
       <Switch>
         <Route path="/chats" component={ChatPage} />
         <Route path="/" component={HomePage} exact />
       </Switch>
     </BrowserRouter>
+  </div>
   );
 }
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Container, Box, Text, Tabs } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      {/* Title Card */}
+      <Box
+        bg="white"
+        w="90%"
+        p="30px"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign="center"
+        m="40px 0 15px 0"
+      >
+        <Text fontSize="4xl" fontWeight="bold">
+          TALK-A-TIVE
+        </Text>
+      </Box>
 
-export default HomePage
+      {/* Tabs Card */}
+      <Box
+        bg="white"
+        w="100%"
+        p="30px"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs.Root defaultValue="login" width="full">
+          <Tabs.List>
+            <Tabs.Trigger value="login">Login</Tabs.Trigger>
+            <Tabs.Trigger value="signup">Sign Up</Tabs.Trigger>
+          </Tabs.List>
+
+          <Box mt="4">
+            <Tabs.Content value="login">
+              Login Form Here
+            </Tabs.Content>
+
+            <Tabs.Content value="signup">
+              Signup Form Here
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
